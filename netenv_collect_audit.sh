1200c7e42f48ea70facc9e4cf0d1bdcc993b36f6
SSA <soesanaung2147@gmail.com>
2024-06-25 15:57:28 +0800

    0225062024

--- netenv_collect_audit.sh
+++ netenv_collect_audit.sh
@@ -34,3 +34,8 @@ auditctl -l | awk '/^ *-w/ \
 &&/ +-p *wa/ \
 &&(/ key= *[!-~]* *$/||/ -k *[!-~]* *$/)'
 }
+
+#remediate
+printf " -a always,exit -F arch=b64 -S sethostname,setdomainname -k system-locale -a always,exit -F arch=b32 -S sethostname,setdomainname -k system-locale -w /etc/issue -p wa -k system-locale -w /etc/issue.net -p wa -k system-locale -w /etc/hosts -p wa -k system-locale -w /etc/sysconfig/network -p wa -k system-locale -w /etc/sysconfig/network-scripts/ -p wa -k system-locale " >> /etc/audit/rules.d/50-system_local.rules
+augenrules --load
+if [[ $(auditctl -s | grep "enabled") =~ "2" ]]; then printf "Reboot required to load rules\n"; fi
