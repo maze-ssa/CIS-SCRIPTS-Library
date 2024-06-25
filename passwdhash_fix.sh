acab104a03be07f825091d1d748c3411fba4f6c6
SSA <soesanaung2147@gmail.com>
2024-06-25 14:34:31 +0800

    0225062024

--- passwdhash_fix.sh
+++ passwdhash_fix.sh
@@ -2,15 +2,13 @@
 { 
         for fn in system-auth password-auth; do 
                 file="/etc/authselect/$(head -1 /etc/authselect/authselect.conf | grep 'custom/')/$fn" 
-                if ! grep -Pq -- '^\h*password\h+(requisite|required|sufficient)\h+pam_unix\.so(\h+[^#\n\r]+)?\h+sha512\b.*$' "$file"; 
-                then 
-                        if grep -Pq -- '^\h*password\h+(requisite|required|sufficient)\h+pam_unix\.so(\h+[^#\n\r]+)?\h+(md5|blowfish|bigcrypt|sha256|yescrypt)\b.*$' "$file"; 
-                        then 
-                        sed -ri 's/(md5|blowfish|bigcrypt|sha256|yescrypt)/sha512/' "$file" else sed -ri 's/(^\s*password\s+(requisite|required|sufficient)\s+pam_unix.so\s+)(.*)$/\1sha512 \3/' "$file" 
-                fi 
+                if ! grep -Pq -- '^\h*password\h+(requisite|required|sufficient)\h+pam_unix\.so(\h+[^#\n\r]+)?\h+sha512\b.*$' "$file"; then 
+                        if grep -Pq -- '^\h*password\h+(requisite|required|sufficient)\h+pam_unix\.so(\h+[^#\n\r]+)?\h+(md5|blowfish|bigcrypt|sha256|yescrypt)\b.*$' "$file"; then 
+                                sed -ri 's/(md5|blowfish|bigcrypt|sha256|yescrypt)/sha512/' "$file" else sed -ri 's/(^\s*password\s+(requisite|required|sufficient)\s+pam_unix.so\s+)(.*)$/\1sha512 \3/' "$file" 
+                fi      
                         fi 
-                        done 
-                                authselect apply-changes 
+        done 
+        authselect apply-changes 
         
 }
 
