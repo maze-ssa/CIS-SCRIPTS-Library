77bf400ec5bea7ee830c3547c1df5e4d9e033836
SSA <soesanaung2147@gmail.com>
2024-06-25 14:13:28 +0800

    0225062024

--- cronauthorized_audit.sh
+++ cronauthorized_audit.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 {
  if rpm -q cronie >/dev/null; then
- [ -e /etc/cron.deny ] && echo "Fail: cron.deny exists"
+    [ -e /etc/cron.deny ] && echo "Fail: cron.deny exists"
  if [ ! -e /etc/cron.allow ]; then
- echo "Fail: cron.allow doesn't exist"
+    echo "Fail: cron.allow doesn't exist"
  else
- ! stat -Lc "%a" /etc/cron.allow | grep -Eq "[0,2,4,6]00" && echo
-"Fail: cron.allow mode too permissive"
- ! stat -Lc "%u:%g" /etc/cron.allow | grep -Eq "^0:0$" && echo "Fail:
-cron.allow owner and/or group not root"
+    ! stat -Lc "%a" /etc/cron.allow | grep -Eq "[0,2,4,6]00" && echo "Fail: cron.allow mode too permissive"
+    ! stat -Lc "%u:%g" /etc/cron.allow | grep -Eq "^0:0$" && echo "Fail:cron.allow owner and/or group not root"
  fi
- if [ ! -e /etc/cron.deny ] && [ -e /etc/cron.allow ] && stat -Lc "%a"
-/etc/cron.allow | grep -Eq "[0,2,4,6]00" \ && stat -Lc "%u:%g" /etc/cron.allow | grep -Eq "^0:0$"; then
- echo "Pass"
+ if [ ! -e /etc/cron.deny ] && [ -e /etc/cron.allow ] && stat -Lc "%a" /etc/cron.allow | grep -Eq "[0,2,4,6]00" \ && stat -Lc "%u:%g" /etc/cron.allow | grep -Eq "^0:0$"; then
+    echo "Pass"
  fi
  else
- echo "Pass: cron is not installed on the system"
+    echo "Pass: cron is not installed on the system"
  fi
 }
