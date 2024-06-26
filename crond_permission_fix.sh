16c6f08f450771c0520222b5bfdff41d2dad2210
SSA <soesanaung2147@gmail.com>
2024-06-26 15:54:58 +0800

    0326052025

---
+++ crond_permission_fix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash 
+{ 
+    if rpm -q cronie >/dev/null; then 
+        [ -e /etc/cron.deny ] && rm -f /etc/cron.deny 
+        [ ! -e /etc/cron.allow ] && touch /etc/cron.allow 
+        chown root:root /etc/cron.allow 
+        chmod u-x,go-rwx /etc/cron.allow 
+    else 
+        echo "cron is not installed on the system" fi 
+}
